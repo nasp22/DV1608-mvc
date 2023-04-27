f5339a63f5c0d4368c67f147c20a1e395710e2ed
Nadja Spångberg <nasp22@student.bth.se>
2023-04-27 15:06:13 +0200

    composer json updated, prepared for tests

---
+++ tests/Card/CardTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class Card.
+ */
+class CardTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object has the expected
+     * properties, use no arguments.
+     */
+    public function testCreateCard()
+    {
+        $card = new Card();
+        $this->assertInstanceOf("\App\Card\Card", $card);
+
+    }
+}
